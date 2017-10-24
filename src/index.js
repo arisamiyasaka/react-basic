097deb46ed885cf0e854d5bab2d83097e8a9e271
Arisa Miyasaka <11729510+arisamiyasaka@users.noreply.github.com>
2017-10-24 20:58:38 +0900

    add work.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './work.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('work'));
 registerServiceWorker();
---
+++ src/work.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+  <h1>Hello React.</h1>,
+  document.getElementById('root')
+)
+
+/** Work
+ReactDOM.render() を使用して、複数のDOMにReactコンポーネントをレンダリング(マッピング)して下さい。
+*/
