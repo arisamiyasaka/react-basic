d20a2c92c4a48716c87b98c0cd44ac2d7b843112
Arisa Miyasaka <11729510+arisamiyasaka@users.noreply.github.com>
2017-10-24 21:43:26 +0900

    Lesson1: Multi Rendering to DOM

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './work.js';
+import Foo from './Work.js'; // const Work = <h1>Hello React.</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('work'));
+ReactDOM.render(<Foo />, document.getElementById('work'));
 registerServiceWorker();
--- src/work.js
+++ src/work.js
@@ -1,7 +1,7 @@
-ReactDOM.render(
-  <h1>Hello React.</h1>,
-  document.getElementById('root')
-)
+import React from 'react'
+
+const Work = () => <h1>Hello React.</h1>
+export default Work
 
 /** Work
 ReactDOM.render() を使用して、複数のDOMにReactコンポーネントをレンダリング(マッピング)して下さい。
