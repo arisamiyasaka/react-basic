067060a7f3367af2e19bda6f8e32210627909205
Arisa Miyasaka <11729510+arisamiyasaka@users.noreply.github.com>
2017-11-03 02:28:02 +0900

    Lesson3: Pass Down Data by Props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Clock, Greet } from './Work.js'; // const Work = <h1>Hello React.</h1>
+import Work from './Work.js'; // const Work = <h1>Hello React.</h1>
 
-ReactDOM.render(<Greet />, document.getElementById('root'));
-ReactDOM.render(<Clock />, document.getElementById('work'));
+ReactDOM.render(<Work time={(new Date()).toLocaleTimeString()} greet={"Hello React!!"} />, document.getElementById('work'));
 registerServiceWorker();
--- src/work.js
+++ src/work.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Clock = () => <h1>{(new Date()).toLocaleTimeString()}.</h1>
-const Greet = () => <h1>OK Google</h1>
-
-export { Clock, Greet }
+const Clock = ({ time }) => {
+  console.log("hello")
+  return (
+    <h1>{time}.</h1>
+  )
+}
+export default Clock
 
 /** Work
-自分自身で自由にコンポーネントを作成して、DOMにレンダリングして下さい。
+• timeというpropsの名前を自由に変更してみて下さい。
+• Clockのアロー関数を、より簡単に表記して下さい。
 */
